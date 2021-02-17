bcfd69210d0faa53c8bf5644eb289aef0f7e8d26
jbolota <franciscocarvalho88@gmail.com>
2021-02-17 10:16:52 +0000

    error page e cards

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <title>Bando - Upsss</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<style type="text/css">
+#background {
+  background-image: url('https://criaturamusic.com/storage/uploads/fundi.jpg');
+  background-repeat: repeat-y;
+}
+
+div{
+  display: grid;
+}
+div img{
+  text-align: center;
+  margin:auto;
+}
+div span{
+  text-align: center;
+  line-height: 40px;
+  font-size:medium;
+  font-family: "Georgia", sans-serif;
+}
+</style>
+</head>
+<body id="background">
+  <div>
+    <img src="https://criaturamusic.com/storage/uploads/titulo.png" />
+    <span><strong>Estamos a atualizar o Bando.</strong><br>Voltamos dentro de momentos.<br>Ainda hoje!<br>Até já</span>
+  </div>
+
+</body>
+</html>
