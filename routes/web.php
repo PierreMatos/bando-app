382e46bcc5c664b398c87a8a93844285adc23649
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2021-02-19 20:12:06 +0000

    again

--- routes/web.php
+++ routes/web.php
@@ -71,7 +71,7 @@ Route::middleware(['auth'])->group(function()
             $exitCode = Artisan::call('cache:clear');
             // return what you want
         });
-        //Reoptimized class loader:
+        //Reoptimized class loader :
         Route::get('/optimize', function() {
             $exitCode = Artisan::call('optimize');
             return '<h1>Reoptimized class loader</h1>';
